09fdd24eb3364f6261277e5215f8fd55eaa90a81
bugra-gokcek <bugragokcek1@gmail.com>
2023-08-28 18:24:48 +0300

    hotfix

--- fastqr.sh
+++ fastqr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-curl -o ~/qr.png "https://chart.apis.google.com/chart?cht=qr&chs=200x200&chld=L|1&chl=$1" 
+curl -o $HOME/qr.png "https://chart.apis.google.com/chart?cht=qr&chs=200x200&chld=L|1&chl=$1" 
 
-if [ -e "~/qr.png" ]; then 
+if [ -e "$HOME/qr.png" ]; then 
    echo -e "\033[1m-----\n\033[92mQR created successfully.\033[37m\n-----"
-   display ~/qr.png & 
+   display $HOME/qr.png & 
    
 else
     echo -e "\033[31m\033[1m***Failed to create QR code! Please be sure you have a stable network connection and try again.***"
