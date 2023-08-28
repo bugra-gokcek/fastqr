eb6a5a404d887c4a7a2b75ac51c9e0ee4618196b
bugra-gokcek <bugragokcek1@gmail.com>
2023-08-28 17:02:39 +0300

    updated script

--- fastqr.sh
+++ fastqr.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
+pid=0
 curl -o /tmp/qr.png "https://chart.apis.google.com/chart?cht=qr&chs=200x200&chld=L|1&chl=$1" 
 
-if [ -e "/tmp/qr.png" ]; then
-   display /tmp/qr.png
+if [ -e "/tmp/qr.png" ]; then 
+   echo -e "\033[1m-----\n\033[92mQR created successfully.\033[37m\n-----"
+   display /tmp/qr.png & 
+   sleep 5
+   rm /tmp/qr.png
+   sleep 4
+   
 else
-    echo "Failed to create QR code. Please try again or be sure to have a stable network connection."
+    echo -e "\033[31m\033[1m***Failed to create QR code! Please be sure you have a stable network connection and try again.***"
 fi
